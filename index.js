7416922ce0cfa773f49abacaeeecfcfb8a9942c8
Antonio Palmeira <av.palmeira@gmail.com>
2021-04-28 16:59:22 -0300

    adapt routes

--- index.js
+++ index.js
@@ -7,36 +7,21 @@ const swaggerDocument = YAML.load('./swagger.yaml');
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); 
 
-app.get('/fahrenheit/:valor/celsius', (req, res) => {
+app.get('/fahrenheit2celsius/:valor', (req, res) => {
 
     let valor = req.params.valor;
     let celsius = (valor - 32) * 5 / 9;
     res.json({ "celsius": celsius, "maquina": os.hostname() });
 });
 
-app.get('/celsius/:valor/fahrenheit', (req, res) => {
+app.get('/celsius2fahrenheit/:valor', (req, res) => {
 
     let valor = req.params.valor;
     let fahrenheit = (valor * 9 / 5) + 32;
     res.json({ "fahrenheit": fahrenheit, "maquina": os.hostname() });
 });
 
-app.get('/temperatura/fahrenheitparacelsius/:valor', (req, res) => {
-
-    let valor = req.params.valor;
-    let celsius = (valor - 32) * 5 / 9;
-    res.json({ "celsius": celsius });
-});
-
-
-app.get('/temperatura/celsiusparafahrenheit/:valor', (req, res) => {
-
-    let valor = req.params.valor;
-    let fahrenheit = (valor * 9 / 5) + 32;
-    res.json({ "fahrenheit": fahrenheit });
-});
-
-
 app.listen(8080, () => {
     console.log("Servidor rodando na porta 8080");
 });
+
